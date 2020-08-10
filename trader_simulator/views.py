dc1d30d8d729e56424247f571b15ca6b5c022cdb
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-10 01:26:03 -0300

    my wallet page

--- trader_simulator/views.py
+++ trader_simulator/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from trader_simulator.models import InvestmentLog
+from trader_simulator.external_data.exchange_apis import MarketData
 
 def new_investment(request):
-    pass
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
 
 def my_wallet(request):
-    pass
+    username = request.user.username
+    user_investments = list(
+        InvestmentLog.objects.values('investment_symbol').\
+        filter(username__exact=username).\
+        annotate(total_quantity=Sum('quantity'))
+    )
+
+    market_data = MarketData()
+
+    total_value = 0
+    for index, investment in enumerate(user_investments):
+        symbol = investment['investment_symbol']
+        quantity = investment['total_quantity']
+        value = market_data.get_value(symbol, quantity)
+        user_investments[index]['value'] = value
+
+        total_value += value
+
+    context = {
+        'investments': user_investments,
+        'total_value': total_value
+    }
+
+    return render(request, 'my_wallet.html', context=context)
