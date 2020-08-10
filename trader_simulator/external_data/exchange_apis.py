9f0761b04ea1b88a51dff960a0981f52799c7cd3
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-09 23:45:31 -0300

    module to interact with external apis to get exchange data

--- trader_simulator/external_data/exchange_apis.py
+++ trader_simulator/external_data/exchange_apis.py
@@ -1,8 +1,13 @@
 """
 Collection of functions to access external exchange info APIs
+Note that this module access 3 different APIs. This is not ideal, as all the data can be found in
+one of them (Alpha Vantage). But, as the free key for this API limits to 5 calls per minute, which
+is too low, the design decision here was to continue using it as font of extra info for the
+investments offered, and use 2 other APIs to get the real time rate.
 """
 import os
 import requests
+from datetime import datetime
 
 class MarketData():
     """
@@ -10,24 +15,66 @@ class MarketData():
     """
 
     def __init__(self):
-        pass
+        self.alpha_vantage_key = os.getenv('ALPHA_VANTAGE_KEY')
+        self.coin_api_key = os.getenv('COIN_API_KEY')
+        self.market_stack_key = os.getenv('MARKET_STACK_KEY')
 
     def get_cryptocurrency_rates(self, symbol):
         """
+        Get cryptocurrency rates for a given symbol, from coinapi.io REST API
         """
-        pass
+        crypto_rate_url = 'https://rest.coinapi.io/v1/exchangerate/{0}/USD/?apikey={1}'
+        request_url = crypto_rate_url.format(symbol, self.coin_api_key)
+        response = requests.get(request_url)
+        response_data = response.json()
+        return response_data
 
     def get_cryptocurrency_info(self, symbol):
         """
+        Get cryptocurrency info for a given symbol, from alpha vantage
         """
-        pass
+        now_datetime = datetime.now()
+        crypto_info_url = "https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol={0}&apikey={1}"
+        request_url = crypto_info_url.format(symbol, self.alpha_vantage_key)
+        response = requests.get(request_url)
+        response_data = response.json()
+
+        # format keys and select needed data
+        cryptocurrency_info = {}
+        cryptocurrency_info['symbol'] = response_data['Crypto Rating (FCAS)']['1. symbol']
+        cryptocurrency_info['name'] = response_data['Crypto Rating (FCAS)']['2. name']
+        cryptocurrency_info['fcas_rating'] = response_data['Crypto Rating (FCAS)']['3. fcas rating']
+        cryptocurrency_info['fcas_score'] = response_data['Crypto Rating (FCAS)']['4. fcas score']
+        cryptocurrency_info['source_datetime'] = now_datetime
+
+        return cryptocurrency_info
 
     def get_stock_rates(self, symbol):
         """
+        Get stock rates for a given symbol, from market stack API
         """
-        pass
+        stock_rate_url = 'http://api.marketstack.com/v1/intraday/latest?symbols={0}&access_key={1}'
+        request_url = stock_rate_url.format(symbol, self.market_stack_key)
+        response = requests.get(request_url)
+        response_data = response.json()
+        return response_data
 
     def get_stock_info(self, symbol):
         """
+        Get stock info for a given symbol, from alpha vantage
         """
-        pass
+        now_datetime = datetime.now()
+        stock_info_url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={0}&apikey={1}"
+        request_url = stock_info_url.format(symbol, self.alpha_vantage_key)
+        response = requests.get(request_url)
+        response_data = response.json()
+
+        # format keys and select needed data
+        stock_info = {}
+        stock_info['symbol'] = response_data['Symbol']
+        stock_info['name'] = response_data['Name']
+        stock_info['gross_profit'] = response_data['GrossProfitTTM']
+        stock_info['quartely_revenue_growth'] = response_data['QuarterlyRevenueGrowthYOY']
+        stock_info['source_datetime'] = now_datetime
+
+        return stock_info
