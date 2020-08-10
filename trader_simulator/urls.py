d695a020ddce9d27fecb7f4d4344bb102502c617
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-10 09:50:42 -0300

    add urls for auth and home

--- trader_simulator/urls.py
+++ trader_simulator/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from trader_simulator.views import new_investment, my_wallet
+from trader_simulator.views import new_investment, my_wallet, home, registration
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'trader_simulator'
 
 urlpatterns = [
+    path('', home, name='home'),
     path('new_investment', new_investment, name='new_investment'),
     path('my_wallet', my_wallet, name='my_wallet'),
+    path('login', LoginView.as_view(template_name='login.html')),
+    path('logout', LogoutView.as_view(template_name='logout.html')),
+    path('registration', registration, name='registration')
 ]
