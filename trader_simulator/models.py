bdf17b88785de4da56fa9d3b5f7b2b24111cd4cf
marcuusmelo <melo.mvinicius@gmail.com>
2020-08-09 23:53:33 -0300

    add models and migrations -- store info for investments daily

--- trader_simulator/models.py
+++ trader_simulator/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
-class InvestmentLog(models.Model):
-    pass
+
+class CryptocurrencyInfo(models.Model):
+    symbol = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=128)
+    fcas_rating = models.CharField(max_length=64)
+    fcas_score = models.FloatField()
+    source_datetime = models.DateTimeField()
+
+class StockInfo(models.Model):
+    symbol = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=128)
+    gross_profit = models.FloatField()
+    quartely_revenue_growth = models.FloatField()
+    source_datetime = models.DateTimeField()
+
+# class InvestmentLog(models.Model):
+#     pass
